98348e19c7a565fcbb969407281a00803a5d3931
root <root@localhost.localdomain>
2014-04-30 17:02:57 -0400

    new

--- lin_epoll/serv.cpp
+++ lin_epoll/serv.cpp
@@ -41,7 +41,7 @@ if((list_sock=socket(AF_INET,SOCK_STREAM,0))<0){  throw 2; };
   
 
  listen(list_sock,1024);
- /*
+
  if((pid=fork())>0){ int fd=open("/var/run/ser.pid", O_RDWR | O_CREAT);
            char bs[16]; sprintf(bs,"%d",pid);
           write(fd,bs,strlen(bs));
@@ -61,17 +61,14 @@ for(i = n-1; i >= 0; --i) {
 dup(std);
 dup(std);
  setuid(p->pw_uid);
- setgid(p->pw_gid);*/
+ setgid(p->pw_gid);
  signal(SIGINT, serv::sig_hand );
  signal(SIGTERM, serv::sig_hand );
  signal(SIGPIPE, SIG_IGN );
  
 
   serv s(list_sock);
-/*  }
- catch(std::bad_alloc& d){ exit(-1); }
- catch(int c){  exit(-1); };
- */
-};
+
+ };
      
 
