9450889db8338860ef00d5a1d06589f600843a98
root <root@localhost.localdomain>
2014-05-09 06:30:43 -0400

    new

--- c.c
+++ c.c
@@ -1,62 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int* m;
+int cmp(void* a, void* b){ return *(int*)a < *(int*)b; };
 typedef unsigned in;
-in pos;
+  void srt(int* st, int* lst, int (*cmp)(void*, void*)){
 
-  inline int part(in len, in p){
-  in l=0;
-  in r=len;
-  in mdl;
-     int mid=m[pos+p];
-
-     while(l!=r){
-
-     while(mid>m[pos+l]){ l++; if(l==r){ return l;}; };
-
-
-        do{ r--; if(l==r){ return l;}; }while(m[pos+r]>mid);
-
-           mdl=m[pos+l]; m[pos+l]=m[pos+r]; m[pos+r]=mdl; 
-     l++;
-    };
-  return l;
+    int *tmp=st, *md=st, *sp;
+   int n=0;
+  int mid=*st;
+   while(tmp==st){ if(md==lst){ return; };
+           sp=lst;
+          
+         while(sp!=tmp){
+          n=0;
+     while(cmp(tmp, md)){ ++tmp; if(tmp==sp){ n=1; break; }; };
+           if(n){ break; };
+     do{ --sp; if(tmp==sp){ n=1; break; }; }while(cmp(md, sp));
+              if(n){ break; };
+           n=*tmp; *tmp=*sp; *sp=n; 
+     ++tmp;
+    };  ++md; mid=*md;
   };
-
-
-  void srt(in len){
-
-   in j;
-   in l;
-
-
-   switch(len){
-  case 2 : if(m[pos+1]<m[pos]){ l=m[pos]; m[pos]=m[pos+1]; m[pos+1]=l; }; pos+=2; return;  
   
-  case 1 : pos++; return;
-
-  default : 
-         l=0; j=0;   
-   while(l==0){ if(j==len){ pos+=len; return; };
-         
-
-              l=part(len,j); j++;
-       }; break;
-    };
-  
-  srt(l);
-   srt(len-l);
-
-    return;
-  };
-
+   srt(st, tmp, cmp);
+   srt(tmp, lst, cmp);
+};
+ 
  int main(int arg, char * s[]){
   int i;
+  int* m;
   in num=atoi(s[1]);
   in out=atoi(s[2]);
- pos=0;
+
    m=malloc(sizeof(int)*num);
   
  for(i=0;i<num;i++)
@@ -64,11 +39,13 @@ in pos;
    m[i]=random();
     };
 
- srt(num);
- 
+  srt(m, m+num-1, cmp);
+
+
+
 for(i=0;i<out;i++)
  {
-  printf("NUM==%d\n",m[i]);
+  printf("NUm==%d\n",m[i]);
   };
 
  free(m);
--- c.cpp
+++ c.cpp
@@ -1,43 +1,69 @@
 #include <iostream>
-#include <string.h>
+#include <string>
+#include <vector>
+#include <iterator>
 
-template<typename Pos, typename Cmp> 
-  inline Pos prt(Pos fr, Pos md, Pos lt, Cmp cmp){
-   
-     while(lt!=fr){
-
-     while(cmp(*fr, *md)){ ++fr; if(lt==fr){ return fr; }; };
-
-        do{ --lt; if(lt==fr){ return fr; }; }while(cmp(*md, *lt));
-
-         std::swap(*fr, *lt);
-     ++fr;
-    };
-  return fr;
-  };
 
 template<typename Pos, typename  Cmp> 
   
-inline  void sort(Pos pt, Pos pm, Cmp cmp){
+inline  void srt(Pos pt, Pos pm, Cmp cmp){
   
-   Pos tmp, m;
-  if((pt+1)==pm){ 
-   if( cmp( *pm , *pt) ){  std::swap(*pt, *pm); }; return; }; 
-  
-  if(pt==pm)  return;
-
-         m=pt;  
-        tmp=pt;
-       
-   while(tmp==pt){ if(m==pm){  return; };
+   Pos tmp=pt, md=pt, sp;
+   int n=0;
+   while(tmp==pt){ if(md==pm){  return; };
          
-     tmp=prt(pt, m++, pm, cmp); 
-       
-      }; 
+           sp=pm;
+          
+         while(sp!=tmp){
+         
+     while(cmp(*tmp, *md)){ ++tmp; if(tmp==sp){ n=1; break; }; };
+           if(n){ break; };
+     do{ --sp; if(tmp==sp){ n=1; break; }; }while(cmp(*md, *sp));
+              if(n){ break; };
+           std::swap(*tmp, *sp);
+     ++tmp;
+    };  ++md;
+  };
+   
+      srt(pt, tmp, cmp);
+      srt(tmp, pm, cmp);
+ };
+
+template<typename Pos> 
   
-      sort(pt, tmp, cmp);
-      sort(tmp, pm, cmp);
+  void srt(Pos pt, Pos pm){
+  
+   Pos tmp=pt, md=pt, sp, lim=pm;
+   int n=0;
+   
+   while(tmp==pt){ if(md==lim){  return; };
+         
+           sp=pm;
+          
+         while(sp!=tmp){
+         
+     while(*tmp < *md){ ++tmp; if(tmp==sp){ n=1; break; }; };
+           if(n){ break; };
+     do{ --sp; if(tmp==sp){ n=1; break; }; }while(*md < *sp);
+              if(n){ break; };
+           std::swap(*tmp, *sp);
+     ++tmp;
+     };  ++md;
+    };
+ srt(pt, tmp);
+ srt(tmp, pm);
+ };
 
-   };
- 
 
+int cmp(int a, int b){ return a<b; }; 
+int main(int a , char** s){
+
+ std::vector<int> v;
+ int sz=std::stoi(s[1]);
+ while(--sz>0){
+
+ v.push_back(random());
+};
+srt(v.begin(), v.end(), cmp);
+ std::copy(v.begin(), v.begin()+std::stoi(s[2]), std::ostream_iterator<int>(std::cout, "\n"));
+};
