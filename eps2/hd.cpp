b5ac4ca9fd37181b9dabeb8115af0c8667815071
root <root@localhost.localdomain>
2014-07-05 15:08:01 -0400

    new

---
+++ eps2/hd.cpp
@@ -0,0 +1,40 @@
+#include <serv.h>
+const str hd::s0="HTTP/1.1 200 OK\r\n";
+const str hd::s1="Date: xxxxxxxxxxxxxxxxxxxxxxxx GMT\r\n";
+const str hd::s2="Server: MATRIX\r\n";
+const str hd::s3="Content-Type: %s; charset=utf-8\r\n";
+const str hd::s4="Content-Length: %d \r\n";
+const str hd::s5="Connection: keep-alive\r\n\r\n";
+const str hd::s6="Connection: close\r\n\r\n";
+const str hd::s7="HTTP/1.1";
+const str hd::s8="Server: MATRIX\r\n";
+const str hd::s9="Content-Type";
+const str hd::s10="content-type";
+const str hd::s11="Content-Length";
+const str hd::s12="Connection";
+const str hd::s13="Keep-Alive";
+const str hd::s14="GET";
+const str hd::s15="POST";
+const str hd::s16="Host";
+const str hd::s17="Date: ";
+const str hd::s18="; charset=utf-8\r\n";
+const str hd::s19=" GMT\r\n";
+const str hd::s20="text/html";
+const str hd::s21="text/javascript";
+const str hd::s22="image/jpeg";
+const str hd::s23="image/gif";
+const str hd::s24="Accept:";
+const str hd::s25="accept:";
+const str hd::s26="connection";
+const str hd::s27="content-length";
+const str hd::s28="Content-Language";
+const str hd::s29="content-language";
+const str hd::s30="host";
+const str hd::s31="\r\n\r\n";
+const str hd::s32="lose";
+const str hd::err_s1="HTTP/1.1 404 Not Found\r\n";
+const str hd::err_s2="HTTP/1.1 500 Internal Server Error\r\n";
+const str hd::err_s3="HTTP/1.1 400 Bad Request\r\n";
+
+
+
---
+++ eps2/http_handler.cpp
@@ -0,0 +1,261 @@
+#include <serv.h>
+
+ void http_handler::send_header(http_conn* c){
+  
+  int sz;
+  char date[25];
+
+  time_t tm_t;
+   time(&tm_t);
+  strncpy(date,asctime(localtime(&tm_t)),25);
+  date[24]='\0';
+   std::ostringstream ost;
+   ost.rdbuf()->pubsetbuf(c->header, 1024);
+   ost.seekp(0);
+      ost<<hd::s0.c_str()
+      <<hd::s2.c_str()
+      <<hd::s17.c_str()
+      <<date<<hd::s19.c_str()  
+      <<hd::s9<<hd::s20.c_str()
+      <<hd::s18.c_str()
+      <<hd::s10.c_str()
+      <<c->buf_size<<"\r\n"
+    <<hd::s5.c_str();
+ 
+  c->header_len=static_cast<int>(ost.tellp());
+
+    };
+
+inline  int http_handlep::req_gen (http_conn* c){
+
+  http_conn* sp=c;
+switch(sp->state){
+ case REQ_HEAD :
+  char* b=sp->buf_recv;
+  char* mid;
+  char* tmp=b;
+  char* end=b+sp->size_recv;
+  int n;
+   while(*b==' '){ b++; if(b==end){ return 1; }; };
+ 
+ do{
+    
+  if(std::equal(b,b+3,hd::s14.c_str())){ sp->request.method=HTTP_GET; b+=3; break; };
+  if(std::equal(b,b+4,hd::s15.c_str())){ sp->request.method=HTTP_POST; b+=4; break; };
+ 
+    }while(1);
+
+  while(*b==' '){ b++; if(b==end){ return 1; }; };
+   n=0;
+   while(*(b+n)!=' '){  n++; };
+
+   sp->request.path=b; b+=n; *b='\0';
+
+
+    do{
+ 
+    tmp=const_cast<char*>(hd::s16.c_str());
+   if((tmp=std::search(b, end, tmp, tmp+4))==end){ 
+ 
+     tmp=const_cast<char*>(hd::s30.c_str());
+   if((tmp=std::search(b, end, tmp, tmp+4))==end){ sp->request.host=NULL; break;  }; 
+      };
+         tmp+=5;
+  while(*tmp==' '){ tmp++; if(tmp==end){ return 1; }; };
+   sp->request.host=tmp;
+    
+ while(*tmp!=' ' && *tmp!='\r'){ tmp++; if(tmp==end){ return 1; }; };
+   *tmp='\0'; break;
+  }while(1);
+ 
+ 
+    tmp=const_cast<char*>(hd::s31.c_str());
+   if((tmp=std::search(b, end, tmp, tmp+4))==end){ return 1; };
+     sp->request.header_size=tmp-sp->buf_recv+4;
+      
+  do{
+      tmp=const_cast<char*>(hd::s9.c_str());
+   if((tmp=std::search(b, end, tmp, tmp+12))==end){ 
+    
+     tmp=const_cast<char*>(hd::s10.c_str());
+   if((tmp=std::search(b, end, tmp, tmp+12))==end){ sp->request.content_type=NULL; break;  }; 
+     
+   }; 
+       
+       tmp+=13;
+     
+    while(*tmp==' '){ tmp++; if(tmp==end){ return 1; }; };
+        
+         sp->request.content_type=tmp;
+          
+    while(*tmp!=' '){ tmp++; if(tmp==end){ return 1; }; }; *tmp='\0'; tmp++;
+    while(*tmp==' '){ tmp++;  if(tmp==end){ return 1; }; }; 
+
+     sp->request.encoding=tmp;
+ 
+    while(*tmp!=' ' || *tmp!='\r'){ tmp++; if(tmp==end){ return 1; }; }; *tmp='\0'; break;
+
+   }while(1);
+ 
+
+  do{
+
+     tmp=const_cast<char*>(hd::s12.c_str());
+
+   if((tmp=std::search(b, end, tmp, tmp+10))==end){ 
+
+      tmp=const_cast<char*>(hd::s26.c_str());
+   if((tmp=std::search(b, end, tmp, tmp+10))==end){ sp->request.connection_keep=0x00; break;  }; 
+      };
+         tmp+=11;
+
+  while(*tmp==' '){ tmp++; if(tmp==end){ return 1; }; };
+   
+    mid=tmp+1;
+
+  while(*tmp!=' ' && *tmp!='\r'){ tmp++; if(tmp==end){ return 1; }; };
+   
+    *tmp='\0'; if(std::equal(mid, mid+4, hd::s32.c_str())){ sp->request.connection_keep=0; sp->keep=0; break; };
+
+      sp->request.connection_keep=1; break;
+ 
+      }while(1);
+
+    do{
+
+      tmp=const_cast<char*>(hd::s28.c_str());
+   if((tmp=std::search(b, end, tmp, tmp+16))==end){ 
+
+      tmp=const_cast<char*>(hd::s29.c_str());
+   if((tmp=std::search(b, end, tmp, tmp+16))==end){ sp->request.content_language=NULL; break;  }; 
+      };
+         tmp+=17;
+  while(*tmp==' '){ tmp++; if(tmp==end){ return 1; }; };
+   sp->request.content_language=tmp;
+  while(*tmp!=' '  && *tmp!='\r'){ tmp++; if(tmp==end){ return 1; }; };
+   *tmp='\0'; break;
+  }while(1); 
+  
+ 
+ if(sp->request.method==HTTP_POST){
+  
+   do{
+
+     tmp=const_cast<char*>(hd::s11.c_str());
+   if((tmp=std::search(b, end, tmp, tmp+14))==end){ 
+
+     tmp=const_cast<char*>(hd::s27.c_str());
+   if((tmp=std::search(b, end, tmp, tmp+14))==end){ sp->request.content_length=NULL; return 1;  }; 
+      
+      };
+         tmp+=15;
+  while(*tmp==' '){ tmp++; if(tmp==end){ return 1; }; };
+  char* point=tmp;
+  while(*tmp!=' '  && *tmp!='\r'){ tmp++; if(tmp==end){ return 1; }; };
+   *tmp='\0';
+  sp->request.content_length=std::stoll(point); break;
+        
+      }while(1);
+   }; 
+ break;
+ };
+ return 0;
+    };
+void http_handlep::handler(conn* s){
+    struct stat st;
+     size_t sz;
+     int error;
+     uint8_t i;
+     cache_t ch;
+    http_conn* cs=static_cast<http_conn*>(s);
+    if(req_gen(cs)){ error_hand(cs, 1); return; };
+   const char *tmp=cs->request.path;
+          
+     if(cs->request.method==HTTP_GET){
+      try{
+          errno=0;
+        i=stat(tmp, &st); if(i<0 || errno==ENOENT){  throw 1; };
+          
+       if(st.st_size>100000){ cs->type=REQ_SENDFILE;
+          cs->buf_size=st.st_size;
+         if((cs->file_fd=open(tmp, O_RDONLY))==0){ throw 1; };
+              }
+               else{ cs->type=0;
+         if(cacher(tmp, ch)){ cs->type=REQ_SENDMSG;
+                              cs->buf_size=st.st_size;
+         if((cs->file_fd=open(tmp, O_RDONLY))==0){ throw 1; };
+             
+            }else{
+               cs->buf_send=ch.pointer.get(); cs->buf_size=ch.size;
+                 };
+               };
+           
+         }catch(int er){  error_hand(cs, er); return; };
+           
+            send_header(cs);          
+           struct iovec* pvc=cs->msg.msg_iov;   
+           cs->msg.msg_iovlen=2;
+           pvc->iov_base=&cs->header;
+           pvc->iov_len=cs->header_len;
+           pvc++;
+           pvc->iov_base=&cs->buf_send;
+           pvc->iov_len=cs->buf_size;
+           cs->state=REQ_WRITE;
+          };
+       };
+  
+int http_handler::cacher(const char* s, cache_t& ch){
+ 
+ cache_t chc;
+ char* p;
+ int len;
+ 
+ key_mp key(s);
+
+  
+  if(( auto it=cache_map.find(key))!=cache_map.end()){ 
+    chc.pointer=it->second.pointer; chc.size=it->second.size; }
+    else{ 
+          std::fstream ifs(s);
+           ifs.seekg (0, ifs.end);
+           len = ifs.tellg();
+           ifs.seekg (0, ifs.beg);
+          
+          try { p=new char[len]; }catch(std::bad_alloc& b){ ifs.close(); return 1; };
+         chc.pointer=std::shared_ptr<char>(p);
+       ifs.read(chc.pointer.get(), len);
+      ifs.close();
+    chc.size=len;
+if(cache_map.insert(std::pair<key_mp, cache_t>(key, chc)).second==false) return 1;
+   };
+  ch.pointer=chc.pointer; ch.size=chc.size;
+
+  return 0;
+ };
+void http_handler::error_hand(http_conn* s, uint8_t er){
+        int error=er;
+         http_conn* cs=s; 
+         switch(error){ case 1 :  hd::err_s1.copy(cs->header, hd::err_s1.size()); 
+                           cs->buf_size=hd::err_s1.size(); cs->buf_send=cs->header;
+                             cs->state=REQ_WRITE; break;
+                        default :  break;
+                      };
+                 cs->request.connection_keep=0;
+            struct iovec* pvc=cs->msg.msg_iov;   
+           
+           cs->msg.msg_iovlen=1;
+           pvc->iov_base=&cs->header;
+           pvc->iov_len=cs->header_len;
+          
+          };
+
+void http_handler::destroy_conn(conn* p){
+  http_conn* s=static_cast<http_conn*>(p);
+   delete s;
+ };
+
+conn* http_handler::init_conn(){
+  http_conn* p=new http_conn();
+   return static_cast<conn*>(p);
+  };
+  
